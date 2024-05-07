57857a0d2696955282a997ddfaf18943fd3299f0
s1f102100704 <s1f102100704@iniad.org>
2024-05-08 01:49:02 +0900

    爆弾10コランダムで生成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,13 +17,40 @@ const Home = () => {
 
   // ]); //userの行動管理
   const [samplePos, setSamplePos] = useState(0);
+  const clickHandler = (x: number, y: number) => {
+    const newBoard = structuredClone(bombmap);
+    makeBomb(newBoard);
+  };
+  const getRandomInt = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  const makeBomb = (newBoard: number[][]) => {
+    let a, b;
+    for (let i = 0; i <= 10; ) {
+      a = getRandomInt(0, 8);
+      b = getRandomInt(0, 8);
+      if (newBoard[a][b] !== 1) {
+        newBoard[a][b] = 1;
+        i++;
+      }
+    }
+  };
   console.log('sample:', samplePos);
   return (
     <div className={styles.container}>
-      <div className={styles.boardstyle}>
-        {bombmap.map((row, y) =>
-          row.map((color, x) => <div className={styles.cellstyle} key={`${x}-${y}`} />),
-        )}
+      <div className={styles.fullboard}>
+        <div className={styles.boardstyle}>
+          <div className={styles.headBoard} />
+          {bombmap.map((row, y) =>
+            row.map((color, x) => (
+              <div
+                className={styles.cellstyle}
+                key={`${x}-${y}`}
+                onClick={() => clickHandler(x, y)}
+              />
+            )),
+          )}
+        </div>
       </div>
       <div
         className={styles.samplestyle}
