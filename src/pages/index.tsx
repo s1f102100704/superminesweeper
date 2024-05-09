3efdc33a8367a22d25b05fb3aeaf9c8a22bbf5cf
s1f102100704 <s1f102100704@iniad.org>
2024-05-09 20:23:42 +0900

    きれそう

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,26 +28,68 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
 
   const [samplePos, setSamplePos] = useState(0);
 
   const clickHandler = (x: number, y: number) => {
     const newBoard = structuredClone(bombmap);
+    const usermap = structuredClone(userInputs);
     if (samplePos === 0) {
       makeBomb(newBoard, x, y);
     }
-
+    makeuserinputs(usermap, x, y);
+    allDirections(newBoard);
+    setUserInputs(usermap);
     setBombmap(newBoard);
     setSamplePos(10);
   };
+  //爆弾の周りの数字
+  const allDirections = (newBoard: number[][]) => {
+    const directions = [
+      { dy: -1, dx: 0 }, // 上
+      { dy: 1, dx: 0 }, // 下
+      { dy: 0, dx: -1 }, // 左
+      { dy: 0, dx: 1 }, // 右
+      { dy: -1, dx: 1 }, // 右斜め上
+      { dy: 1, dx: 1 }, // 右斜め下
+      { dy: -1, dx: -1 }, // 左斜め上
+      { dy: 1, dx: -1 }, // 左斜め下
+    ];
+    directions.forEach((direction) => {
+      makeNum(newBoard, direction.dx, direction.dy);
+    });
+  };
 
+  const makeNum = (newBoard: number[][], dx: number, dy: number) => {
+    for (let y = 0; y <= 8; y++) {
+      for (let x = 0; x <= 8; x++) {
+        if (newBoard[y + dy][x + dx] === 1) {
+        }
+      }
+    }
+  };
+
+  //ユーザーの動作
+  const makeuserinputs = (usermap: number[][], x: number, y: number) => {
+    usermap[y][x] = 1;
+  };
   //整数のランダム生成
   const getRandomInt = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
   //爆弾の配置
   const makeBomb = (newBoard: number[][], x: number, y: number) => {
-    console.log(total);
     let a, b;
     for (let i = 0; i <= 10; ) {
       a = getRandomInt(0, 8);
@@ -65,7 +107,7 @@ const Home = () => {
       <div className={styles.fullboard}>
         <div className={styles.boardstyle}>
           <div className={styles.headBoard} />
-          {bombmap.map((row, y) =>
+          {board.map((row, y) =>
             row.map((color, x) => (
               <div
                 className={styles.cellstyle}
