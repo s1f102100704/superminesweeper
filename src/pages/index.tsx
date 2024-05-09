79a0c20cbcf3db3d2683a489f0df95c6a3d8425d
s1f102100704 <s1f102100704@iniad.org>
2024-05-09 17:43:41 +0900

    初回のみ爆弾の生成が行われるように

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,29 +13,53 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]); //clickhandler を使って初回かどうか
-  // const [userInputs,setUserInputs]=([
+  // 0 未クリック
+  // 1 左クリック
+  // 2 はてな
+  // 3 旗
+  const [userInputs, setUserInputs] = useState<(0 | 1 | 2 | 3)[][]>([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
 
-  // ]); //userの行動管理
   const [samplePos, setSamplePos] = useState(0);
+
   const clickHandler = (x: number, y: number) => {
     const newBoard = structuredClone(bombmap);
-    makeBomb(newBoard);
+    if (samplePos === 0) {
+      makeBomb(newBoard, x, y);
+    }
+
+    setBombmap(newBoard);
+    setSamplePos(10);
   };
+
+  //整数のランダム生成
   const getRandomInt = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
-  const makeBomb = (newBoard: number[][]) => {
+  //爆弾の配置
+  const makeBomb = (newBoard: number[][], x: number, y: number) => {
+    console.log(total);
     let a, b;
     for (let i = 0; i <= 10; ) {
       a = getRandomInt(0, 8);
       b = getRandomInt(0, 8);
-      if (newBoard[a][b] !== 1) {
-        newBoard[a][b] = 1;
-        i++;
+      if (a !== y || b !== x) {
+        if (newBoard[a][b] !== 1) {
+          newBoard[a][b] = 1;
+          i++;
+        }
       }
     }
   };
-  console.log('sample:', samplePos);
   return (
     <div className={styles.container}>
       <div className={styles.fullboard}>
@@ -47,15 +71,19 @@ const Home = () => {
                 className={styles.cellstyle}
                 key={`${x}-${y}`}
                 onClick={() => clickHandler(x, y)}
-              />
+              >
+                {color === 1 && (
+                  <div
+                    className={styles.bombstyle}
+                    style={{ backgroundPosition: `${-30 * samplePos}px  0px` }}
+                  />
+                )}
+              </div>
             )),
           )}
         </div>
       </div>
-      <div
-        className={styles.samplestyle}
-        style={{ backgroundPosition: `${-30 * samplePos}px  0px` }}
-      />
+      <div style={{ backgroundPosition: `${-30 * samplePos}px  0px` }} />
 
       <button onClick={() => setSamplePos((p) => (p + 1) % 14)}>sample</button>
     </div>
