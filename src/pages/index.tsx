5ef7cac8d68816af823dbfef8b049a9873bdf309
s1f102100704 <s1f102100704@iniad.org>
2024-05-16 12:35:03 +0900

    代替元に戻した

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,6 @@ import { withRouter } from 'next/router';
 import styles from './index.module.css';
 import { useState } from 'react';
 
-const directions = [
-  { dy: -1, dx: 0 }, // 上
-  { dy: 1, dx: 0 }, // 下
-  { dy: 0, dx: -1 }, // 左
-  { dy: 0, dx: 1 }, // 右
-  { dy: -1, dx: 1 }, // 右斜め上
-  { dy: 1, dx: 1 }, // 右斜め下
-  { dy: -1, dx: -1 }, // 左斜め上
-  { dy: 1, dx: -1 }, // 左斜め下
-];
 const Home = () => {
   const [bombmap, setBombmap] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -52,12 +42,114 @@ const Home = () => {
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
   ];
-
+  const directions = [
+    { dy: -1, dx: 0 }, // 上
+    { dy: 1, dx: 0 }, // 下
+    { dy: 0, dx: -1 }, // 左
+    { dy: 0, dx: 1 }, // 右
+    { dy: -1, dx: 1 }, // 右斜め上
+    { dy: 1, dx: 1 }, // 右斜め下
+    { dy: -1, dx: -1 }, // 左斜め上
+    { dy: 1, dx: -1 }, // 左斜め下
+  ];
   const newBombmap = structuredClone(bombmap);
   const usermap = structuredClone(userInputs);
   const clickHandler = (x: number, y: number) => {
+    first(usermap, newBombmap, x, y);
+    clicked(usermap, x, y);
+    console.log('爆弾', newBombmap);
+    console.log('usermap', usermap);
     setUserInputs(usermap);
+  };
+  const judgeUsermap = (usermap: number[][], newBombmap: number[][]) => {
+    for (let p = 0; p <= 8; p++) {
+      for (let q = 0; q <= 8; q++) {
+        if (usermap[p][q] === 1 && board[p][q] === -1) {
+          directions.forEach((direction) => {
+            makeNum(usermap, newBombmap, q, p, direction.dx, direction.dy);
+          });
+          if (board[p][q] === -1) {
+            directions.forEach((direction) => {
+              nearWhite(usermap, newBombmap, q, p, direction.dx, direction.dy);
+            });
+
+            console.log('use', usermap);
+          }
+        }
+      }
+    }
+    for (let p = 0; p <= 8; p++) {
+      for (let q = 0; q <= 8; q++) {
+        if (usermap[p][q] === 1) {
+          white(p, q);
+        }
+      }
+    }
+  };
+  //user側の処理
+  const clicked = (usermap: number[][], x: number, y: number) => {
+    usermap[y][x] = 1;
+  };
+  const makeNum = (
+    usermap: number[][],
+    newBombmap: number[][],
+    x: number,
+    y: number,
+    dx: number,
+    dy: number,
+  ) => {
+    if (y + dy >= 0 && y + dy <= 8 && newBombmap[y + dy][x + dx] === 1) {
+      board[y][x] += 1;
+    }
+  };
+  const nearWhite = (
+    usermap: number[][],
+    newBombmap: number[][],
+    x: number,
+    y: number,
+    dx: number,
+    dy: number,
+  ) => {
+    if (y + dy >= 0 && y + dy <= 8 && newBombmap[y + dy][x + dx] !== 1) {
+      clicked(usermap, x + dx, y + dy);
+    }
+  };
+  const white = (p: number, q: number) => {
+    const element = document.getElementById(`${q}-${p}`);
+    if (element) {
+      element.style.backgroundColor = 'white';
+    }
+  };
+  judgeUsermap(usermap, newBombmap);
+
+  //爆弾を作る
+  const makeBomb = (newBombmap: number[][], x: number, y: number) => {
+    for (let i = 0; i <= 10; ) {
+      const p = getRandomInt();
+      const q = getRandomInt();
+      if (newBombmap[p][q] !== 1 && (p !== y || q !== x)) {
+        newBombmap[p][q] = 1;
+        i++;
+      }
+    }
     setBombmap(newBombmap);
+  };
+  const getRandomInt = () => {
+    return Math.floor(Math.random() * 9);
+  };
+  //初めてのクリック
+  const first = (usermap: number[][], newBombmap: number[][], x: number, y: number) => {
+    let count = 0;
+    for (let p = 0; p <= 8; p++) {
+      for (let q = 0; q <= 8; q++) {
+        if (usermap[p][q] === 1) {
+          count += 1;
+        }
+      }
+    }
+    if (count === 0) {
+      makeBomb(newBombmap, x, y);
+    }
   };
 
   return (
